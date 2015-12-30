07bd68e8ee7233a66d18efc0e6b99d1dc6ebaa07
jennykittyli <lij9@bxscience.edu>
2015-12-30 17:58:33 -0500

    Added Animation and snowshooter scripts and designed dungeon

---
+++ Assets/Cha_Data/Script/AnimationScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScript : MonoBehaviour {
+	
+	public Animation animator; 
+	public GameObject knight; 
+	// Use this for initialization
+	void Start () {
+	
+		animator = GetComponent<Animation> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		toggleAnimations (); 
+	}
+
+	void toggleAnimations() {
+		if (Input.GetButtonDown ("Vertical")) {
+			animator.Play ("Walk"); 
+		}
+		if (Input.GetButtonDown ("Fire1")) {
+			animator.Play ("Attack"); 
+		} 
+	}
+
+}
+
---
+++ Assets/Cha_Data/Script/SnowShooter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowShooter : MonoBehaviour
+{
+	public Rigidbody projectile;
+	public float speed = 40;
+	float loadRate = 0.5f; // how often a new projectile can be fired
+	float timeRemaining; // how much time before the next shot can happen
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timeRemaining -= Time.deltaTime;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
+		if (Input.GetButton("Fire1")&& timeRemaining <= 0 && Physics.Raycast(ray))
+		{
+			timeRemaining = loadRate; 
+			Rigidbody snowball = Instantiate(projectile,transform.position,transform.rotation)as Rigidbody;
+			snowball.velocity = transform.TransformDirection(new Vector3(0, 0,speed));
+		}
+	}
+}
