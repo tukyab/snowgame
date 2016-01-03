d5de97c82e9c764ff445423e3378ae14522866f8
jennykittyli <lij9@bxscience.edu>
2016-01-02 23:52:38 -0500

    Added Teleporter script and colliders to dungeon
    NEED TO FIX: Character controller detaches from knight upon teleporting

---
+++ Assets/Cha_Data/Script/Teleporter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+	public Transform exit;
+	static Transform last;
+	
+	void OnTriggerEnter ( Collider other )
+	{
+		if ( exit == last )
+			return;
+		TeleportToExit( other );
+	}
+	void OnTriggerExit ( )
+	{
+		if ( exit == last )
+			last = null;
+	}
+	void TeleportToExit ( Collider other )
+	{
+		last = transform;
+		other.transform.position = exit.transform.position;
+	}
+	
+	
+}
