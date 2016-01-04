086af84a33d223eb41495c972803f0ab69879ab4
Tenzin Ukyab <tukyab@Tenzins-MBP.home>
2016-01-04 00:13:29 -0500

    finish

--- Assets/Cha_Data/Script/Knight.cs
+++ Assets/Cha_Data/Script/Knight.cs
@@ -37,6 +37,7 @@ public class Knight : MonoBehaviour {
 		if (health == 0) {
 			KnightAnimator.Play("Dead");
 			Destroy (gameObject);
+			Destroy (other.gameObject);
 		}
 	}
 
@@ -44,7 +45,8 @@ public class Knight : MonoBehaviour {
 		GUIStyle style = new GUIStyle ();
 		style.fontSize = 16;
 		style.normal.textColor = Color.blue;
-		 
+		GUI.Label (new Rect (50, 10, 250, 100), "Snow Game by Tenzin and Jenny", style);
+		GUI.Label (new Rect (50, 30, 250, 100), "'w' to move forward, mouse to turn, click to shoot", style);
 		if (health == 0) {
 			GUI.Label (new Rect (50, 50, 250, 100), "Game Over", style);
 		} else if (slime.getHealth() == 0) {
--- Assets/Cha_Data/Script/Snowman.cs
+++ Assets/Cha_Data/Script/Snowman.cs
@@ -5,7 +5,7 @@ public class Snowman : MonoBehaviour {
 
 	public GameObject projectile;
 	public AudioClip throwingSound;
-	int time = 10000;
+	int time = 0;
 	AudioSource ActionAudio;
 	public GameObject knight;
 
@@ -17,14 +17,15 @@ public class Snowman : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		time -= 1; 
-		if (time % 100 == 10)
+		time += 1; 
+		if (time % 100 == 1)
 		{
 			Vector3 position= new Vector3(knight.transform.position.x, knight.transform.position.y, knight.transform.position.z);  
-			Vector3 startPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 20, gameObject.transform.position.z);
+			Vector3 startPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, gameObject.transform.position.z);
 
 			GameObject snowball= Instantiate (projectile, startPos, Quaternion.identity) as GameObject; 
 			snowball.transform.LookAt(position); 
+			snowball.AddComponent<Rigidbody>();
 			snowball.GetComponent<Rigidbody>().AddForce(snowball.transform.forward *5000); 
 			ActionAudio.Play();
 		}
